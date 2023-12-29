585428b7203b101189702eafde89b201f6ef36b6
Vanh1811 <anhnguyenviet065@gmail.com>
2023-12-29 23:37:32 +0700

    UI added to prj

--- main.js
+++ main.js
@@ -1,75 +1,93 @@
-function getComputerChoice(){
+function getComputerChoice() {
     let computer = Math.floor(Math.random() * 3);
-    if(computer === 0){
+    if (computer === 0) {
         return 'rock';
-    }
-    else if(computer === 1){
+    } else if (computer === 1) {
         return 'paper';
-    }
-    else if(computer === 2){
+    } else if (computer === 2) {
         return 'scissors';
     }
 }
 
-function gamePlay(playerSelection, computerSelection){
-    if(playerSelection.toLowerCase() === computerSelection){
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    if (playerSelection.toLowerCase() === computerSelection) {
         return 'tie';
-    }
-    else{
-        if (playerSelection.toLowerCase() === 'rock'){
-            if(computerSelection === 'paper'){
-                return 'computer';
-            }
-            else{
-                return "player";
-            }
-        }
-        else if(playerSelection.toLowerCase() === 'paper'){
-                if(computerSelection === 'scissors'){
-                    return "computer";
-                }
-                else{
-                    return "player";
-                }
-        }
-        else if(playerSelection.toLowerCase() === 'scissors'){
-            if(computerSelection === 'rock'){
-                return "computer";
-            }
-            else{
-                return "player";
-            }
+    } else {
+        if (playerSelection.toLowerCase() === 'rock') {
+            return computerSelection === 'paper' ? 'computer' : 'player';
+        } else if (playerSelection.toLowerCase() === 'paper') {
+            return computerSelection === 'scissors' ? 'computer' : 'player';
+        } else if (playerSelection.toLowerCase() === 'scissors') {
+            return computerSelection === 'rock' ? 'computer' : 'player';
         }
     }
 }
 
-function game(){
-    let rounds = 5;
+function game() {
     let playerWins = 0;
     let computerWins = 0;
-    while(rounds--){
-        const computerSelection = getComputerChoice();
-        let playerSelection = prompt("Rock, Paper, Scissors?");
-        let result = gamePlay(playerSelection, computerSelection);
-        if(result === 'tie'){
-            rounds++;
-        }
-        else if(result === 'computer'){
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+
+    const rockBtn = document.createElement('button');
+    const scissorsBtn = document.createElement('button');
+    const paperBtn = document.createElement('button');
+
+    rockBtn.textContent = "ROCK";
+    scissorsBtn.textContent = "SCISSORS";
+    paperBtn.textContent = "PAPER";
+
+    const buttonStyle = 'padding: 10px 20px; margin: 5px; font-size: 16px; cursor: pointer;';
+    rockBtn.setAttribute('style', buttonStyle);
+    scissorsBtn.setAttribute('style', buttonStyle);
+    paperBtn.setAttribute('style', buttonStyle);
+
+    rockBtn.addEventListener('click', () => {
+        updateGame('rock');
+    });
+
+    scissorsBtn.addEventListener('click', () => {
+        updateGame('scissors');
+    });
+
+    paperBtn.addEventListener('click', () => {
+        updateGame('paper');
+    });
+
+    const displayResult = document.createElement('div');
+
+    const displayStyle = 'font-size: 18px; margin-top: 10px;'
+    displayResult.setAttribute('style', displayStyle)
+    displayResult.style.display = 'flex';
+    displayResult.style.flexDirection = 'column';
+    displayResult.style.alignItems = 'center';
+    displayResult.style.marginTop = '10px';
+
+    function updateGame(playerSelection) {
+        let result = playRound(playerSelection);
+
+        if (result === 'computer') {
             computerWins++;
-        }
-        else if(result === 'player'){
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        } else if (result === 'player') {
             playerWins++;
         }
+
+        displayResult.textContent = `Player: ${playerWins} Computer: ${computerWins}`;
+
+        if (playerWins === 5) {
+            displayResult.textContent = 'Winner: Player';
+        } else if (computerWins === 5) {
+            displayResult.textContent = 'Winner: Computer';
+        }
     }
 
-    if(playerWins > computerWins){
-        console.log('Winner: Player');
-    }
-    else{
-        console.log('Winner: Computer');
-    }
+    const miniContainer = document.createElement('div');
+    miniContainer.appendChild(rockBtn);
+    miniContainer.appendChild(scissorsBtn);
+    miniContainer.appendChild(paperBtn);
+
+    const container = document.querySelector('#game-container');
+    container.appendChild(miniContainer);
+    container.appendChild(displayResult);
 }
 
-game();
+document.addEventListener('DOMContentLoaded', game());
